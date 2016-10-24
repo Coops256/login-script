ff2c2d5c1f4bfacf6b5d5d7f2c7f58d764c41bbe
Coops256 <cooper.rockwell.cr@gmail.com>
2016-10-24 15:29:37 -0600

    Ease of installation scripts
    requires .ini with file paths in order of {home, startup.bat,logon.bat,launchers.bat}

--- InstallFromGit.bat
+++ InstallFromGit.bat
@@ -1,15 +1,5 @@
 set repo="https://github.com/Coops256/login-script"
-set /p tempDest="where to clone the repo?"
-set /p startup="where to place startup.bat?"
-set /p logon="where to place logon.ps1?"
-set /p launchers="where to place javaLauncher.bat and scriptLauncher.bat?"
-
-git clone %repo% %tempDest%
-
-cd %tempDest%
-
-move %tempDest%\startup.bat %startup%
-move %tempDest%\logon.ps1 %logon%
-move %tempDest%\*.bat %launchers%
-
-set /p exit="Press any key to exit"
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" -Scope CurrentUser
+git clone %repo% %~dp0\Repo
+powershell.exe -noexit "& '.\setDirs.ps1'"
+set /p exit="Press enter to exit"
