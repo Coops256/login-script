9111342c783e61f0c88d4257fb58d159ec5f85f1
Coops256 <cooper.rockwell.cr@gmail.com>
2016-10-24 15:36:48 -0600

    Delete InstallFromGit.bat

--- InstallFromGit.bat
+++
@@ -1,5 +0,0 @@
-set repo="https://github.com/Coops256/login-script"
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" -Scope CurrentUser
-git clone %repo% %~dp0\Repo
-powershell.exe -noexit "& '.\setDirs.ps1'"
-set /p exit="Press enter to exit"
